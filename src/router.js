e2387b3b156b90ba8d096ece4df14f7ac712d462
rash <rashfael@isobeef.org>
2020-06-29 21:35:38 +0200

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,11 @@
+import { defineAsyncComponent } from "vue";
+import {
+  createRouter,
+  createWebHistory
+} from "vue-router/dist/vue-router.esm.js";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: []
+});
+export default router;
